133a210e90fcf0ae1a702ccac5232ba957baa59a
yunji <y00ndlek@gmail.com>
2023-11-04 18:13:02 +0900

    landing_page_test

---
+++ landing_page/script.js
@@ -0,0 +1,15 @@
+const swiper = new Swiper(".swiper", {
+  // centerSlide
+  slidesPerView: "1.1",
+  centeredSlides: false,
+  spaceBetween: 40,
+
+  //loop
+  loop: false,
+
+  //nextSlide
+  mousewheel: true,
+
+  //slideSpeed
+  speed: 2000,
+});
