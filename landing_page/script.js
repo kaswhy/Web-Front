220c32948be5633bb6613ddf0d41ec77e0411c64
taeyeon2280 <52042235+taeyeon2280@users.noreply.github.com>
2023-11-07 00:30:34 +0900

    feat : 스크롤 감지기능 추가, page3,4 태그명 수정

--- landing_page/script.js
+++ landing_page/script.js
@@ -13,3 +13,17 @@ const swiper = new Swiper(".swiper", {
   //slideSpeed
   speed: 2000,
 });
+
+
+window.addEventListener("scroll", function() {
+  const image = document.getElementById("arrow");
+  const scrollPosition = window.scrollY;
+  const bottomOfPage = document.body.scrollHeight - window.innerHeight;
+
+  if (scrollPosition >= bottomOfPage - 5) {
+    image.style.transform = "translateY(5rem)";
+  } else {
+    image.style.transform = "translateY(0)";
+  }
+});
+
