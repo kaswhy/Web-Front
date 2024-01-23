2b34935196d2a3483ea43ac4ce8c6d6e44979d5e
yunji Kim <y00ndlek@gmail.com>
2024-01-23 21:40:57 +0900

    chore: swiper 속도 변경

--- script.js
+++ script.js
@@ -11,21 +11,19 @@ const swiper = new Swiper(".swiper", {
   mousewheel: true,
 
   //slideSpeed
-  speed: 2000,
+  speed: 1500,
 });
 
-
-window.addEventListener("scroll", function() {
+window.addEventListener("scroll", function () {
   const image = document.getElementById("arrow");
   const scrollPosition = window.scrollY;
   const bottomOfPage = document.body.scrollHeight - window.innerHeight;
 
   if (scrollPosition >= bottomOfPage - 5) {
-    image.style.transition = "visibility 0.1s"
+    image.style.transition = "visibility 0.1s";
     image.style.visibility = "hidden";
   } else {
-    image.style.transition = "visibility 0.1s"
-    image.style.visibility="visible";
+    image.style.transition = "visibility 0.1s";
+    image.style.visibility = "visible";
   }
 });
-
